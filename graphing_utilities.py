6bdfde7f15c8f034e51efc1d4f40084a40a7f781
Jake O'Rielly <jake.orielly@gmail.com>
2020-01-29 03:28:51 +0000

    Coloring and legend to make graph more readable

--- graphing_utilities.py
+++ graphing_utilities.py
@@ -2,21 +2,24 @@ import yfinance as yf
 import matplotlib.pyplot as plt 
 from pandas import DataFrame
 
+colors = ['blue','green','red','cyan','magenta','yellow','black','white']
+
 def compare(stocks):
     # Create the plot space upon which to plot the data
     fig, ax = plt.subplots()
     fig.autofmt_xdate()
     
     tickers = []
-    for i in stocks:
-        tickers.append(yf.Ticker(i))
+    for i in range(len(stocks)):
+        tickers.append(yf.Ticker(stocks[i]))
         data = tickers[-1].history('ytd')
-        ax.plot(data.index.values, data['Close'], '-o',color='blue')
+        ax.plot(data.index.values, data['Close'], '-o',color=colors[i % len(colors)])
 
-    #ax.legend(['Close','High','Low'])
+    ax.legend(stocks)
+
     # Set title and labels for axes
-    ax.set(xlabel="Date",
-    ylabel="Price",
-    title="Close Price")
+    ax.set(xlabel='Date',
+    ylabel='Price',
+    title='Close Price')
 
     plt.savefig('results.png')
