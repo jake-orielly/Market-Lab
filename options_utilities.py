43c5b14d2592d80c335867d45004534a1cba3065
Jake O'Rielly <jake.orielly@gmail.com>
2020-02-15 03:33:59 +0000

    Added break even

--- options_utilities.py
+++ options_utilities.py
@@ -16,14 +16,22 @@ def get_within_percent(tickerName,date,percent,contractType):
     option_df = get_option_chain(ticker,date,contractType)
     curr_price = ticker.history('1d')['Close'][0]
     add_percent(option_df,curr_price)
+    add_break_even(option_df)
     print(option_df)
     max_strike = curr_price + curr_price * percent/100
     min_strike = curr_price - curr_price * percent/100
     matching = option_df.loc[(option_df['strike'] >= min_strike) & (option_df['strike'] <= max_strike),['strike','lastPrice']]
     return matching
 
+# Adds percent of current price as column
 def add_percent(df,curr_price):
     percent = []
     for i in range(len(df)):
         percent.append((df['strike'][i]/curr_price))
     df['percentOfCurr'] = percent 
+
+def add_break_even(df):
+    break_even = []
+    for i in range(len(df)):
+        break_even.append(df['strike'][i] - df['lastPrice'][i])
+    df['breakEven'] = break_even 
